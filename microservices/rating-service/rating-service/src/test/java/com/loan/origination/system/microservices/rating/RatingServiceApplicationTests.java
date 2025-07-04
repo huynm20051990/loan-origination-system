736344e8c3a6345871df47ac6e1f369b8f75bba2
huynm20051990 <huynm20051990@gmail.com>
2025-07-04 15:38:46 +0700

    Initialize microservices

---
+++ microservices/rating-service/rating-service/src/test/java/com/loan/origination/system/microservices/rating/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.origination.system.microservices.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
