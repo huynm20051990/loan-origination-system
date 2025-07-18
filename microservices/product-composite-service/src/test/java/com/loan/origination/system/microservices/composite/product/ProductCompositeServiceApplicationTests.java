7378e91350303aa0e3a39cb45ab981f50755b83d
huynm20051990 <huynm20051990@gmail.com>
2025-07-18 14:46:23 +0700

    Add integration test for microservices

--- microservices/product-composite-service/src/test/java/com/loan/origination/system/microservices/composite/product/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/com/loan/origination/system/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -1,12 +1,109 @@
 package com.loan.origination.system.microservices.composite.product;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+import com.loan.origination.system.api.core.product.Product;
+import com.loan.origination.system.api.core.rating.Rating;
+import com.loan.origination.system.api.core.review.Review;
+import com.loan.origination.system.api.exceptions.InvalidInputException;
+import com.loan.origination.system.api.exceptions.NotFoundException;
+import com.loan.origination.system.microservices.composite.product.integration.ProductCompositeIntegration;
+import java.util.Collections;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductCompositeServiceApplicationTests {
 
-  /*@Test
-  void contextLoads() {
-  }*/
+  private static final int PRODUCT_ID_OK = 1;
+  private static final int PRODUCT_ID_NOT_FOUND = 2;
+  private static final int PRODUCT_ID_INVALID = 3;
 
+  @Autowired private WebTestClient client;
+  @MockitoBean private ProductCompositeIntegration integration;
+
+  @BeforeEach
+  void setup() {
+    when(integration.getProduct(PRODUCT_ID_OK))
+        .thenReturn(new Product(PRODUCT_ID_OK, "name", "name-" + PRODUCT_ID_OK, "mock-address"));
+    when(integration.getRatings(PRODUCT_ID_OK))
+        .thenReturn(
+            Collections.singletonList(
+                new Rating(PRODUCT_ID_OK, 1, "author", 1, "content", "mock-address")));
+    when(integration.getReviews(PRODUCT_ID_OK))
+        .thenReturn(
+            Collections.singletonList(
+                new Review(PRODUCT_ID_OK, 1, "author", "subject", "content", "mock-address")));
+
+    when(integration.getProduct(PRODUCT_ID_NOT_FOUND))
+        .thenThrow(new NotFoundException("NOT FOUND: " + PRODUCT_ID_NOT_FOUND));
+
+    when(integration.getProduct(PRODUCT_ID_INVALID))
+        .thenThrow(new InvalidInputException("INVALID: " + PRODUCT_ID_INVALID));
+  }
+
+  @Test
+  void contextLoads() {}
+
+  @Test
+  void getProductById() {
+    client
+        .get()
+        .uri("/product-composite/" + PRODUCT_ID_OK)
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.productId")
+        .isEqualTo(PRODUCT_ID_OK)
+        .jsonPath("$.ratings.length()")
+        .isEqualTo(1)
+        .jsonPath("$.reviews.length()")
+        .isEqualTo(1);
+  }
+
+  @Test
+  void getProductNotFound() {
+    client
+        .get()
+        .uri("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isNotFound()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.path")
+        .isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+        .jsonPath("$.message")
+        .isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
+  }
+
+  @Test
+  void getProductInvalid() {
+    client
+        .get()
+        .uri("/product-composite/" + PRODUCT_ID_INVALID)
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.path")
+        .isEqualTo("/product-composite/" + PRODUCT_ID_INVALID)
+        .jsonPath("$.message")
+        .isEqualTo("INVALID: " + PRODUCT_ID_INVALID);
+  }
 }
--- microservices/rating-service/src/test/java/com/loan/origination/system/microservices/rating/RatingServiceApplicationTests.java
+++ microservices/rating-service/src/test/java/com/loan/origination/system/microservices/rating/RatingServiceApplicationTests.java
@@ -1,13 +1,112 @@
 package com.loan.origination.system.microservices.rating;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RatingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired private WebTestClient client;
 
+  @Test
+  void getRatingsByProductId() {
+    int productId = 1;
+    client
+        .get()
+        .uri("/rating?productId=" + productId)
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.length()")
+        .isEqualTo(3)
+        .jsonPath("$[0].productId")
+        .isEqualTo(productId);
+  }
+
+  @Test
+  void getRatingsMissingParameter() {
+    client
+        .get()
+        .uri("/rating")
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isEqualTo(BAD_REQUEST)
+        .expectHeader()
+        .contentType(APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.path")
+        .isEqualTo("/rating")
+        .jsonPath("$.message")
+        .isEqualTo("Required query parameter 'productId' is not present.");
+  }
+
+  @Test
+  void getRatingsInvalidParameter() {
+    client
+        .get()
+        .uri("/rating?productId=no-integer")
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isEqualTo(BAD_REQUEST)
+        .expectHeader()
+        .contentType(APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.path")
+        .isEqualTo("/rating")
+        .jsonPath("$.message")
+        .isEqualTo("Type mismatch.");
+  }
+
+  @Test
+  void getRatingsNotFound() {
+    int productIdNotFound = 113;
+
+    client
+        .get()
+        .uri("/rating?productId=" + productIdNotFound)
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.length()")
+        .isEqualTo(0);
+  }
+
+  @Test
+  void getRatingsInvalidParameterNegativeValue() {
+    int productIdInvalid = -1;
+
+    client
+        .get()
+        .uri("/rating?productId=" + productIdInvalid)
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isEqualTo(UNPROCESSABLE_ENTITY)
+        .expectHeader()
+        .contentType(APPLICATION_JSON)
+        .expectBody()
+        .jsonPath("$.path")
+        .isEqualTo("/rating")
+        .jsonPath("$.message")
+        .isEqualTo("Invalid productId: " + productIdInvalid);
+  }
+
+  @Test
+  void contextLoads() {}
 }
