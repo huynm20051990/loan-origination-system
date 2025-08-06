bbc16b8810e3f659b6675495e70c6772fd4b4938
huynm20051990 <huynm20051990@gmail.com>
2025-08-06 13:51:35 +0700

    Logging DB connection url for product service, rating service, review service

--- microservices/rating-service/src/main/java/com/loan/origination/system/microservices/rating/RatingServiceApplication.java
+++ microservices/rating-service/src/main/java/com/loan/origination/system/microservices/rating/RatingServiceApplication.java
@@ -1,14 +1,23 @@
 package com.loan.origination.system.microservices.rating;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.loan.origination.system")
 public class RatingServiceApplication {
 
+  public static final Logger LOG = LoggerFactory.getLogger(RatingServiceApplication.class);
+
   public static void main(String[] args) {
-    SpringApplication.run(RatingServiceApplication.class, args);
+    ConfigurableApplicationContext ctx =
+        SpringApplication.run(RatingServiceApplication.class, args);
+    String mongoDBHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+    String mongoDBPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+    LOG.info("Connected to MongoDB: " + mongoDBHost + ":" + mongoDBPort);
   }
 }
