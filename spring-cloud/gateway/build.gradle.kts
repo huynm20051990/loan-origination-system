91c8181e02896bafb651748f63853289ce9ecdf9
huynm20051990 <huynm20051990@gmail.com>
2025-09-09 14:33:53 +0700

    Set up Spring Cloud Gateway server

---
+++ spring-cloud/gateway/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.loan.origination.system.springcloud"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+var springCloudVersion = "2025.0.0"
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
