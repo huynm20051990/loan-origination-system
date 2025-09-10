7231a96df88340813fc832d66c014f0849638e31
huynm20051990 <huynm20051990@gmail.com>
2025-09-10 14:39:44 +0700

    Miss declaring a WebClient.Builder bean

--- spring-cloud/gateway/src/main/java/com/loan/origination/system/springcloud/gateway/HealthCheckConfiguration.java
+++ spring-cloud/gateway/src/main/java/com/loan/origination/system/springcloud/gateway/HealthCheckConfiguration.java
@@ -1,5 +1,8 @@
 package com.loan.origination.system.springcloud.gateway;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.logging.Level;
 
 @Configuration
 public class HealthCheckConfiguration {
@@ -33,9 +32,9 @@ public class HealthCheckConfiguration {
 
     final Map<String, ReactiveHealthIndicator> registry = new LinkedHashMap<>();
 
-    registry.put("product",           () -> getHealth("http://product"));
-    registry.put("rating",    () -> getHealth("http://rating"));
-    registry.put("review",            () -> getHealth("http://review"));
+    registry.put("product", () -> getHealth("http://product"));
+    registry.put("rating", () -> getHealth("http://rating"));
+    registry.put("review", () -> getHealth("http://review"));
     registry.put("product-composite", () -> getHealth("http://product-composite"));
 
     return CompositeReactiveHealthContributor.fromMap(registry);
@@ -44,10 +43,13 @@ public class HealthCheckConfiguration {
   private Mono<Health> getHealth(String baseUrl) {
     String url = baseUrl + "/actuator/health";
     LOG.debug("Setting up a call to the Health API on URL: {}", url);
-    return webClient.get().uri(url).retrieve().bodyToMono(String.class)
-            .map(s -> new Health.Builder().up().build())
-            .onErrorResume(ex -> Mono.just(new Health.Builder().down(ex).build()))
-            .log(LOG.getName(), Level.FINE);
+    return webClient
+        .get()
+        .uri(url)
+        .retrieve()
+        .bodyToMono(String.class)
+        .map(s -> new Health.Builder().up().build())
+        .onErrorResume(ex -> Mono.just(new Health.Builder().down(ex).build()))
+        .log(LOG.getName(), Level.FINE);
   }
-
 }
