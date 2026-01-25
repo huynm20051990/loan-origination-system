1ad5d8216101a26a5687be02ebadafc8014e40d1
huynm20051990 <huynm20051990@gmail.com>
2026-01-25 13:16:18 +0700

    Modify app

--- app-ui/src/app/app.routes.ts
+++ app-ui/src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { HomeListingsComponent } from './components/home-listings/home-listings'
 import { LoanApplicationComponent } from './components/loan-application/loan-application';
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent } from './components/register/register';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard';
+import { LoanOfficerDashboardComponent } from './components/loan-officer-dashboard/loan-officer-dashboard';
+import { LoanDetailsComponent } from './components/loan-details/loan-details';
+import { OfficerReviewComponent } from './components/officer-review/officer-review';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'listings', pathMatch: 'full' }, // Default to listings
   { path: 'listings', component: HomeListingsComponent },
-  { path: 'apply', component: LoanApplicationComponent },
+  { path: 'apply/:id', component: LoanApplicationComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'user-dashboard', component: UserDashboardComponent },
+  { path: 'officer-dashboard', component: LoanOfficerDashboardComponent },
+  { path: 'loan-details/:id', component: LoanDetailsComponent },
+  { path: 'officer-review/:id', component: OfficerReviewComponent},
 ];
--- app-ui/src/app/app.ts
+++ app-ui/src/app/app.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'; // <--- Check this line
+import { AuthService } from './services/auth';
+import { AsyncPipe, NgIf, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
-  templateUrl: './app.html',      // Check that this matches your filename
-  styleUrl: './app.scss'          // Check that this matches your filename
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    AsyncPipe,
+    NgIf,
+    CommonModule
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
 export class AppComponent {
+  authService = inject(AuthService);
   title = 'loan-ui';
 }
