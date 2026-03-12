0d01420d4631eae6e8abfa5e3cd095857bf72f91
huynm20051990 <huynm20051990@gmail.com>
2026-03-12 14:28:02 +0700

    Init assessment-mcp-server

---
+++ mcp-server/assessment-mcp-server/src/main/java/com/loan/origination/system/mcpserver/assessment/AssessmentMCPServerApplication.java
@@ -0,0 +1,12 @@
+package com.loan.origination.system.mcpserver.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentMCPServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AssessmentMCPServerApplication.class, args);
+  }
+}
---
+++ mcp-server/assessment-mcp-server/src/test/java/com/loan/origination/system/mcpserver/assessment/AssessmentMCPServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.loan.origination.system.mcpserver.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentMCPServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
